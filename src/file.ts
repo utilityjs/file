838caaaf515b8669efd510cac4aee3ddc39d99c8
Shivaji Varma Pusapati Venkata <contact@shivajivarma.com>
2024-06-01 18:05:00 +0530

    feat: read and write -> text and json files

--- src/file.ts
+++ src/file.ts
@@ -1,3 +1,12 @@
+import {JSONArray, JSONObject} from "@utility/types";
+import {copy as fsCopy, expandGlob} from "@std/fs";
+
+/**
+ * Checks if a file or directory exists at the given path.
+ * @param path - The path to the file or directory.
+ * @returns A promise that resolves to true if the file or directory exists, false otherwise.
+ * @throws Will throw an error if an unexpected error occurs, such as a permissions error.
+ */
 export async function exists(
   path: string | URL,
 ): Promise<boolean> {
@@ -16,18 +25,87 @@ export async function exists(
   }
 }
 
-export async function mkdir(filePath: string): Promise<void> {
-  await Deno.mkdir(filePath);
+/**
+ * Creates a new directory at the given path.
+ * @param path - The path where the directory should be created.
+ * @returns A promise that resolves when the directory has been created.
+ * @throws Will throw an error if an unexpected error occurs, such as a permissions error.
+ */
+export async function mkdir(path: string): Promise<void> {
+  await Deno.mkdir(path);
 }
 
-export async function mkdirRecursive(filePath: string): Promise<void> {
-  await Deno.mkdir(filePath, { recursive: true });
+/**
+ * Creates a new directory at the given path, including any necessary parent directories.
+ * @param path - The path where the directory should be created.
+ * @returns A promise that resolves when the directory has been created.
+ * @throws Will throw an error if an unexpected error occurs, such as a permissions error.
+ */
+export async function mkdirRecursive(path: string): Promise<void> {
+  await Deno.mkdir(path, { recursive: true });
 }
 
+/**
+ * Removes a file or directory at the given path.
+ * @param path - The path to the file or directory to remove.
+ * @returns A promise that resolves when the file or directory has been removed.
+ * @throws Will throw an error if an unexpected error occurs, such as a permissions error.
+ */
 export async function remove(path: string): Promise<void> {
   await Deno.remove(path);
 }
 
+/**
+ * Removes a directory and its contents at the given path.
+ * @param path - The path to the directory to remove.
+ * @returns A promise that resolves when the directory and its contents have been removed.
+ * @throws Will throw an error if an unexpected error occurs, such as a permissions error.
+ */
 export async function removeRecursive(path: string): Promise<void> {
   await Deno.remove(path, { recursive: true });
+}
+
+export async function readFile(filePath: string): Promise<Uint8Array> {
+  return await Deno.readFile(filePath);
+}
+
+export async function readTextFile(filePath: string): Promise<string> {
+  return await Deno.readTextFile(filePath);
+}
+
+export async function readJsonFile(filePath: string): Promise<JSONObject> {
+  return JSON.parse(await readTextFile(filePath));
+}
+
+export async function readJsonFilesFromPath(
+  filePath: string,
+): Promise<JSONObject[]> {
+  const result: JSONObject[] = [];
+  const iterator = expandGlob(filePath);
+  for await (const file of iterator) {
+    const data = await readJsonFile(file.path);
+    result.push(data);
+  }
+  return result;
+}
+
+export async function writeTextFile(
+  filePath: string,
+  content: string,
+): Promise<void> {
+  await Deno.writeTextFile(filePath, content);
+}
+
+export async function writeJsonFile(
+  filePath: string,
+  object: JSONObject,
+): Promise<void> {
+  return await writeTextFile(filePath, JSON.stringify(object));
+}
+
+export async function copy(
+  sourcePath: string,
+  destinationPath: string,
+): Promise<void> {
+  await fsCopy(sourcePath, destinationPath);
 }
--- test/mod.test.ts
+++ test/mod.test.ts
@@ -1,15 +1,8 @@
-import { path } from "../deps.ts";
-import {
-  afterAll,
-  assertEquals,
-  beforeAll,
-  describe,
-  it,
-} from "../test_deps.ts";
-import {exists, mkdirRecursive, removeRecursive} from "../mod.ts";
+import * as path from "@std/path";
+import {afterAll, assertEquals, beforeAll, describe, it,} from "../test_deps.ts";
+import {copy, exists, mkdirRecursive, readJsonFile, readJsonFilesFromPath, readTextFile, removeRecursive, writeJsonFile, writeTextFile,} from "../mod.ts";
 
-const __dirname = path.dirname(path.fromFileUrl(import.meta.url));
-const TEST_RESOURCES_PATH = path.normalize(__dirname + "/resources");
+const TEST_RESOURCES_PATH = path.normalize(import.meta.dirname + "/resources");
 
 describe({
   name: "file",
@@ -23,51 +16,51 @@ describe({
       }
     });
 
-    afterAll(async ()=> {
+    afterAll(async () => {
       const copyDir = `${TEST_RESOURCES_PATH}/copy`;
       if (await exists(copyDir)) {
         await removeRecursive(copyDir);
       }
     });
 
-    /*it("#readTextFileSync()", function () {
-      const fileContent = FileUtils.readTextFileSync(
+    it("#readTextFile()", async () => {
+      const fileContent = await readTextFile(
         `${TEST_RESOURCES_PATH}/sample.txt`,
       );
       assertEquals(fileContent, "Hello");
     });
 
-    it("#readJsonFileSync()", function () {
-      const obj = FileUtils.readJsonFileSync(
+    it("#readJsonFile()", async () => {
+      const obj = await readJsonFile(
         `${TEST_RESOURCES_PATH}/sample.json`,
       );
       assertEquals(obj.title, "Hello");
     });
 
-    it("#writeJsonFileSync()", function () {
-      FileUtils.writeJsonFileSync(`${TEST_RESOURCES_PATH}/.temp/write.json`, {
+    it("#writeJsonFile()", async () => {
+      await writeJsonFile(`${TEST_RESOURCES_PATH}/.temp/write.json`, {
         title: "Write",
       });
-      const obj = FileUtils.readJsonFileSync(
+      const obj = await readJsonFile(
         `${TEST_RESOURCES_PATH}/.temp/write.json`,
       );
       assertEquals(obj.title, "Write");
     });
 
-    it("#writeTextFileSync()", function () {
-      FileUtils.writeTextFileSync(
+    it("#writeTextFile()", async () => {
+      await writeTextFile(
         `${TEST_RESOURCES_PATH}/.temp/write.txt`,
         "Write",
       );
-      const str = FileUtils.readTextFileSync(
+      const str = await readTextFile(
         `${TEST_RESOURCES_PATH}/.temp/write.txt`,
       );
       assertEquals(str.toString(), "Write");
     });
 
-    it("#readJsonFilesFromPathSync()", function () {
-      const arr = FileUtils.readJsonFilesFromPathSync(
-        `${TEST_RESOURCES_PATH}/path/!**.json`,
+    it("#readJsonFilesFromPath()", async () => {
+      const arr = await readJsonFilesFromPath(
+        `${TEST_RESOURCES_PATH}/path/**.json`,
       );
       assertEquals(arr[0].title, "a");
       assertEquals(arr[1].title, "b");
@@ -75,38 +68,36 @@ describe({
 
     it("#remove()", async () => {
       const dir = `${TEST_RESOURCES_PATH}/.temp`;
-      FileUtils.removeSync(dir, { recursive: true });
-      assertEquals(FileUtils.existsSync(dir), false);
+      await removeRecursive(dir);
+      assertEquals(await exists(dir), false);
     });
 
-    it("#copySync()", function () {
-      FileUtils.copySync(
+    it("#copy()", async () => {
+      await copy(
         TEST_RESOURCES_PATH + "/path",
         TEST_RESOURCES_PATH + "/copy",
       );
-      const objs = FileUtils.readJsonFilesFromPathSync(
-        TEST_RESOURCES_PATH + "/copy/!**.json",
+      const objs = await readJsonFilesFromPath(
+        TEST_RESOURCES_PATH + "/copy/*.json",
       );
       assertEquals(objs[0].title, "a");
       assertEquals(objs[1].title, "b");
     });
 
-    it("#unZipFromURL()", async () => {
-      FileUtils.mkdirSync(path.join(`${TEST_RESOURCES_PATH}/.temp`), {
-        recursive: true,
-      });
-      await FileUtils.unZipFromURL(
+    /*it("#unZipFromURL()", async () => {
+      await mkdirRecursive(path.join(`${TEST_RESOURCES_PATH}/.temp`));
+      await await unZipFromURL(
         new URL("./resources/compress.zip", import.meta.url),
         path.join(`${TEST_RESOURCES_PATH}/.temp/uncompressed`),
       );
       assertEquals(
-        FileUtils.existsSync(
+        await exists(
           TEST_RESOURCES_PATH + "/.temp/uncompressed/path/a.json",
         ),
         true,
       );
       assertEquals(
-        FileUtils.existsSync(
+        await exists(
           TEST_RESOURCES_PATH + "/.temp/uncompressed/sample.txt",
         ),
         true,
